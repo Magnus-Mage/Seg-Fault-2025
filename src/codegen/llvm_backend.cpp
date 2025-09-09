1e93f9b586a07ef40188291c4845766fef58ec42
Magnus-Mage <romescha123@gmail.com>
2025-09-09 04:41:21 +0000

    Add: fixes for now

--- src/codegen/llvm_backend.cpp
+++ src/codegen/llvm_backend.cpp
@@ -18,7 +18,7 @@
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Target/TargetMachine.h>
 #include <llvm/Target/TargetOptions.h>
-#include <llvm/Support/TargetRegistry.h>
+#include <llvm/MC/TargetRegistry.h>
 #include <llvm/Support/Host.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/raw_ostream.h>
